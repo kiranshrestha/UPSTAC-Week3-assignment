033eb275c004696742d91d81fea60e5174088f16
Kiran Shrestha <kiranshrestha619@gmail.com>
2021-09-15 22:55:46 +0530

    Test try.

--- src/test/java/org/upgrad/upstac/testrequests/ConsultationControllerTest.java
+++ src/test/java/org/upgrad/upstac/testrequests/ConsultationControllerTest.java
@@ -2,22 +2,25 @@ package org.upgrad.upstac.testrequests;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.web.server.ResponseStatusException;
 import org.upgrad.upstac.testrequests.TestRequest;
 import org.upgrad.upstac.testrequests.consultation.ConsultationController;
 import org.upgrad.upstac.testrequests.consultation.CreateConsultationRequest;
+import org.upgrad.upstac.testrequests.lab.CreateLabResult;
 import org.upgrad.upstac.testrequests.lab.TestStatus;
 import org.upgrad.upstac.testrequests.RequestStatus;
 import org.upgrad.upstac.testrequests.TestRequestQueryService;
 
+import java.util.Objects;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
@@ -40,7 +43,7 @@ class ConsultationControllerTest {
         TestRequest testRequest = getTestRequestByStatus(RequestStatus.LAB_TEST_COMPLETED);
 
         //Implement this method
-
+        TestRequest testRequest1 = consultationController.assignForConsultation(testRequest.requestId);
         //Create another object of the TestRequest method and explicitly assign this object for Consultation using assignForConsultation() method
         // from consultationController class. Pass the request id of testRequest object.
 
@@ -50,6 +53,8 @@ class ConsultationControllerTest {
         // make use of assertNotNull() method to make sure that the consultation value of second object is not null
         // use getConsultation() method to get the lab result
 
+        assertEquals(testRequest.requestId, testRequest1.requestId);
+        assertEquals(testRequest1.getStatus(), RequestStatus.DIAGNOSIS_IN_PROCESS);
 
     }
 
@@ -68,10 +73,12 @@ class ConsultationControllerTest {
 
         // Create an object of ResponseStatusException . Use assertThrows() method and pass assignForConsultation() method
         // of consultationController with InvalidRequestId as Id
-
+        Throwable throwable = assertThrows(ResponseStatusException.class, (Executable) consultationController.assignForConsultation(InvalidRequestId));
 
         //Use assertThat() method to perform the following comparison
-        //  the exception message should be contain the string "Invalid ID"
+        //  the exception message should contain the string "Invalid ID"
+
+        assertThat(throwable.getMessage(),matchesPattern("Invalid ID"));
 
     }
 
@@ -124,15 +131,19 @@ class ConsultationControllerTest {
     public void calling_updateConsultation_with_invalid_empty_status_should_throw_exception(){
 
         TestRequest testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
-
+        testRequest.consultation.setSuggestion(null);
         //Implement this method
 
         //Create an object of CreateConsultationRequest and call getCreateConsultationRequest() to create the object. Pass the above created object as the parameter
         // Set the suggestion of the above created object to null.
+        CreateConsultationRequest createConsultationRequest = getCreateConsultationRequest(testRequest);
+
 
         // Create an object of ResponseStatusException . Use assertThrows() method and pass updateConsultation() method
         // of consultationController with request Id of the testRequest object and the above created object as second parameter
         //Refer to the TestRequestControllerTest to check how to use assertThrows() method
+
+        assertThrows(ResponseStatusException.class,(Executable) consultationController.updateConsultation(testRequest.requestId,createConsultationRequest));
 
 
     }
@@ -143,6 +154,11 @@ class ConsultationControllerTest {
         // if the lab result test status is Positive, set the doctor suggestion as "HOME_QUARANTINE" and comments accordingly
         // else if the lab result status is Negative, set the doctor suggestion as "NO_ISSUES" and comments as "Ok"
         // Return the object
+
+        CreateConsultationRequest createConsultationRequest = getCreateConsultationRequest(testRequest);
+
+
+
 
 
         return null; // Replace this line with your code
